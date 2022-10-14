3efe7e1b3ca4995afedd522a75c276b54971644d
Brisa-Codes <brisamukunde1@gmail.com>
2022-10-14 11:31:01 +0300

    smart contracts

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,6 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x54201399A9d3b2148bD0640ab9AB6fe7d60650B9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
